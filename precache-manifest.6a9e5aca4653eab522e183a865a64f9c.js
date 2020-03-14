fba0e3956e9a21fbb8f975f4e4a0d7bdd574e834
Tokihagen <noisullinasiemit@gmail.com>
2020-03-14 20:57:45 +0900

    Updates

---
+++ precache-manifest.6a9e5aca4653eab522e183a865a64f9c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced18f1d0b87659e614767ea958f594b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b0c4af88905fcefcd4e",
+    "url": "./static/css/main.753aa642.chunk.css"
+  },
+  {
+    "revision": "eefed49f6070b78d939e",
+    "url": "./static/js/2.909e2597.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.909e2597.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b0c4af88905fcefcd4e",
+    "url": "./static/js/main.931d67c7.chunk.js"
+  },
+  {
+    "revision": "283c3bf8ca524f20e2ff",
+    "url": "./static/js/runtime-main.db65efd9.js"
+  },
+  {
+    "revision": "4c6d272b7f368adb76054af29ba8ca21",
+    "url": "./static/media/love.4c6d272b.png"
+  },
+  {
+    "revision": "a0fad80aded9ae8f4fcde966beaadfe8",
+    "url": "./static/media/no_img.a0fad80a.png"
+  },
+  {
+    "revision": "28a74918516edb8f57e1438b6127240f",
+    "url": "./static/media/orange.28a74918.png"
+  },
+  {
+    "revision": "f5d0649c4b60d0b136e068721a1aaa7e",
+    "url": "./static/media/rice_ball.f5d0649c.png"
+  },
+  {
+    "revision": "f8b65bbe7e5f2f28725ee45f5619d0c0",
+    "url": "./static/media/run_left.f8b65bbe.png"
+  },
+  {
+    "revision": "58a3c7079fec1bc1994f970cd912988d",
+    "url": "./static/media/run_left2.58a3c707.png"
+  },
+  {
+    "revision": "fed0dd59f9b21692657d0456713a493c",
+    "url": "./static/media/run_right.fed0dd59.png"
+  },
+  {
+    "revision": "26d48eea10cfaefc66a5480e9cd915ad",
+    "url": "./static/media/run_right2.26d48eea.png"
+  },
+  {
+    "revision": "e072d6c25222d7c2fa21e9ee012a9c87",
+    "url": "./static/media/sit.e072d6c2.png"
+  },
+  {
+    "revision": "87199342b5f30e143b51678f14c58799",
+    "url": "./static/media/stand.87199342.png"
+  }
+]);
